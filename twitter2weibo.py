5ae42c34c2d8575f443656f3655d97ddd5962182
herrkaefer <gloolar@gmail.com>
2019-05-07 15:07:46 -0500

    refactory

--- twitter2weibo.py
+++ twitter2weibo.py
@@ -8,125 +8,118 @@ import tweepy
 import weibo
 import pickle
 from datetime import datetime, timedelta
-import urllib
+import urllib.request
 import os
 import sys
 import time
 import appconfig as cfg
-
-
-# Remove temp pidfile and exit
-def exitapp():
-    print("delete pidfile and exit.")
-    os.unlink(pidfile)
-    sys.exit()
+from pid.decorator import pidfile
 
 
 print("--------------------------------------")
 print("Run at: " + str(datetime.now()) + "\n")
 
 # ----------------------------------------------------------------------------
-# To make sure the script is not running twice at the same time
-
-pidfile = "/tmp/twitter2weibo.pid"
-if os.path.isfile(pidfile):
-    print("pid file exists. exit.")
-    sys.exit()
-file(pidfile, 'w').write(str(os.getpid()))
-
-# ----------------------------------------------------------------------------
 # Load last creation dates
 
+here = os.path.dirname(os.path.abspath(__file__))
+pkfile = os.path.join(here, cfg.pkfile)
 needDump = False
-
 try:
-    with open(cfg.pkfile, 'rb') as fi:
+    with open(pkfile, 'rb') as fi:
         records = pickle.load(fi)
     for id in cfg.twitter_ids:
         if records.get(id) is None:
-            records[id] = {'last_date': datetime.now() - timedelta(hours=8.5)}
+            records[id] = {'last_date': datetime.now() - timedelta(hours=8)}
             needDump = True
 except EnvironmentError:
     needDump = True
     records = {}
     for id in cfg.twitter_ids:
-        records[id] = {'last_date': datetime.now() - timedelta(hours=8.5)}
+        records[id] = {'last_date': datetime.now() - timedelta(hours=8)}
 finally:
     if needDump:
-        with open(cfg.pkfile, 'wb') as fi:
+        with open(pkfile, 'wb') as fi:
             pickle.dump(records, fi)
 
+
 # ----------------------------------------------------------------------------
-# Collect new tweets
+def fetch_recent_tweets(from_datetime):
+    """Collect new tweets"""
+    t_auth = tweepy.OAuthHandler(cfg.t_consumer_key, cfg.t_consumer_secret)
+    t_auth.set_access_token(cfg.t_access_token, cfg.t_access_token_secret)
+    t_api = tweepy.API(t_auth)
 
-t_auth = tweepy.OAuthHandler(cfg.t_consumer_key, cfg.t_consumer_secret)
-t_auth.set_access_token(cfg.t_access_token, cfg.t_access_token_secret)
-t_api = tweepy.API(t_auth)
+    tweets = []
 
-tweets = []
-
-for user_id in cfg.twitter_ids:
     try:
-        print("checking user with id: " + user_id)
-        for status in tweepy.Cursor(t_api.user_timeline, id=user_id).items():
-            # print("created at: " + str(status.created_at))
-            if status.created_at <= records[user_id]['last_date']:
-                break
+        for user_id in cfg.twitter_ids:
+            print("checking user with id: " + user_id)
+            for status in tweepy.Cursor(
+                    t_api.user_timeline, id=user_id).items():
+                print("created at: " + str(status.created_at))
+                if status.created_at <= records[user_id]['last_date'] \
+                   or status.created_at < from_datetime:
+                    break
 
-            author_id = status.author.id_str
-            # media = status.extended_entities.get('media')
-            media = status.entities.get('media')
+                author_id = status.author.id_str
+                # media = status.extended_entities.get('media')
+                media = status.entities.get('media')
 
-            if (author_id == user_id) and (media is not None) and (not status.text.startswith('RT')):
-                tweets.append({
-                    'author_id': author_id,
-                    'author_screen_name': status.author.screen_name,
-                    'text': status.text,
-                    'media_urls': [m['media_url'] for m in media],
-                    'creation_date': status.created_at})
+                if (author_id == user_id
+                   and media is not None
+                   and (not status.text.startswith('RT'))):
+                    tweets.append({
+                        'author_id': author_id,
+                        'author_screen_name': status.author.screen_name,
+                        'text': status.text,
+                        'media_urls': [m['media_url'] for m in media],
+                        'creation_date': status.created_at})
     except Exception as e:
         print(e)
-        print("Error happened. Exit.")
-        exitapp()
+        return []
 
-if len(tweets) == 0:
-    print("no new tweets.")
-    exitapp()
-else:
     print("%d new tweets got." % len(tweets))
-    # print(tweets)
+    return tweets
+
 
 # ----------------------------------------------------------------------------
-# Post to Weibo, and update last date records
-
-w_client = weibo.Client(cfg.w_api_key, cfg.w_api_secret,
-                        cfg.w_redirect_uri, cfg.w_token)
-
-for tweet in reversed(tweets):
-    print("\npost to weibo: ")
-    print(tweet)
-    # w_status = tweet['text']  # + ' (RT @' + tweet['author_screen_name'] + ')'
-    w_status = tweet['text'] + ' herrkaefer.com '  # 加安全域名
-
+def post_to_weibo(tweets):
+    """Post to Weibo, and update last date records"""
+    w_client = weibo.Client(cfg.w_api_key, cfg.w_api_secret,
+                            cfg.w_redirect_uri, cfg.w_token)
     try:
-        urllib.urlretrieve(tweet['media_urls'][0], 'temp.jpg')
-        w_client.post(
-            'statuses/share', status=w_status, pic=open('temp.jpg', 'rb'))
-        # Udpate last tweet date for author_id
-        records[tweet['author_id']]['last_date'] = tweet['creation_date']
+        for tweet in reversed(tweets):
+            print("\npost to weibo: ")
+            # w_status = tweet['text']  # + ' (RT @' + tweet['author_screen_name'] + ')'
+            w_status = tweet['text'] + ' ' + cfg.secure_domain + ' '
+            print(w_status)
+            urllib.request.urlretrieve(tweet['media_urls'][0], 'temp.jpg')
+            w_client.post(
+                'statuses/share', status=w_status, pic=open('temp.jpg', 'rb'))
+            # Udpate last tweet date for author_id
+            records[tweet['author_id']]['last_date'] = tweet['creation_date']
+            print("wait a few minutes.")
+            time.sleep(60*3)
 
     except Exception as e:
         print(e)
-        if not str(e).startswith("20021"):  # 20021 content is illegal!
-            exitapp()
+        raise
 
     finally:
         print("Update records to disk.")
-        with open(cfg.pkfile, 'wb') as fi:
+        with open(pkfile, 'wb') as fi:
             pickle.dump(records, fi)
         # Wait some time
-        print("wait a few minutes.")
-        time.sleep(60*3)  # 3 minutes
 
-print("done.")
-exitapp()
+    print("done.")
+
+
+@pidfile(piddir=here)
+def main():
+    tweets = fetch_recent_tweets(datetime.now() - timedelta(hours=8.5))
+    post_to_weibo(tweets)
+
+
+if __name__ == '__main__':
+    main()
